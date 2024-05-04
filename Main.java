27bc8eb250eec7ca39f8a5f96e7ffa41c06e5394
Pawel Stanieta <pawelas000@gmail.com>
2024-05-04 09:04:18 +0200

    3

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.interfaces.Messenger;
 import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,8 +10,11 @@ public class Main {
             Person person = new Person("Alice", 30);
             System.out.println("Person: " + person.getName() + ", Age: " + person.getAge());
 
+            int sum = MathUtils.add(5, 10);
+            System.out.println("Sum of 5 + 10 = " + sum);
+
             Messenger messenger = new EmailMessenger();
-            messenger.sendMessage("Hello from " + person.getName() + "!");
+            messenger.sendMessage("Hello from " + person.getName() + "! The sum is " + sum);
         } catch (InvalidAgeException e) {
             System.err.println("Error: " + e.getMessage());
         }
