ff1c27f3a893234afbd60d4bb6cb2c8ad8b7ea9a
Pawel Stanieta <pawelas000@gmail.com>
2024-05-04 10:06:37 +0200

    2

--- Main.java
+++ Main.java
@@ -7,14 +7,14 @@ import company.utils.MathUtils;
 public class Main {
     public static void main(String[] args) {
         try {
-            Person person = new Person("Alice", 30);
-            System.out.println("Osoba: " + person.getName() + ", Wiek: " + person.getAge());
+            Person person = new Person("Dylan", 24);
+            System.out.println("Osoba: " + person.getName() + "\nWiek: " + person.getAge());
 
             int sum = MathUtils.add(5, 10);
             System.out.println("Suma liczb: 5 + 10 = " + sum);
 
             Messenger messenger = new EmailMessenger();
-            messenger.sendMessage("Pozdrowienia od " + person.getName() + "! Suma liczb to " + sum);
+            messenger.sendMessage("Pozdrowienia od " + person.getName() + "! Suma liczb to " + sum + ".");
         } catch (InvalidAgeException e) {
             System.err.println("Error: " + e.getMessage());
         }
