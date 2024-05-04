8732fbd99fbed69bd0415edb9cae4805c540d800
Pawel Stanieta <pawelas000@gmail.com>
2024-05-04 08:57:17 +0200

    2

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
 import company.beans.Person;
 import company.exceptions.InvalidAgeException;
+import company.interfaces.Messenger;
+import company.implementations.EmailMessenger;
 
 public class Main {
     public static void main(String[] args) {
         try {
             Person person = new Person("Alice", 30);
             System.out.println("Person: " + person.getName() + ", Age: " + person.getAge());
+
+            Messenger messenger = new EmailMessenger();
+            messenger.sendMessage("Hello from " + person.getName() + "!");
         } catch (InvalidAgeException e) {
             System.err.println("Error: " + e.getMessage());
         }
