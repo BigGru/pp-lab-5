a7d75a1ee9b1cdaa21bddd4f65f2765004e1bb00
Pawel Stanieta <pawelas000@gmail.com>
2024-05-04 08:42:58 +0200

    1

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
-public class Main {
+import company.beans.Person;
+import company.exceptions.InvalidAgeException;
 
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Person person = new Person("Alice", 30);
+            System.out.println("Person: " + person.getName() + ", Age: " + person.getAge());
+        } catch (InvalidAgeException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
 }
